0c85d6df6297d368ab3c5ead85621310075c46c4
Or Kreizman <Or.kreizman@mail.huji.ac.il>
2023-12-15 17:36:13 +0200

    update lambda function, change to use s3 resource instead of client

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,23 @@ import json
 import boto3
 from datetime import datetime
 
+
 def lambda_handler(event, context):
     bucket_name = event['bucket name'] if 'bucket name' in event else 'niceexambucket'
-    s3_client = boto3.client('s3')
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-    if response and 'Contents' in response:
-        objects = response['Contents']
-        objects = json.dumps([obj['Key'] for obj in objects])
-    else:
-        objects = f'No objects found in bucket{bucket_name}'
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+    objects_keys = [obj.key for obj in bucket.objects.all()]
+    if len(objects_keys) == 0:  # bucket is empty
+        objects_keys = [f'No objects found in bucket{bucket_name}']
+    objects_keys = json.dumps(objects_keys)
+
+    # give each object name that includes his creation time
     cur_time = datetime.now().strftime("%b %d, %Y %H:%M:%S")
-    s3_client.put_object(Body=objects.encode('utf-8'), Bucket=bucket_name, Key=f'bucket_objects_at:{cur_time}.json')
+    new_object_name = f'bucket_objects_at:{cur_time}.json'
+
+    bucket.put_object(Key=new_object_name, Body=objects_keys)
+
     return {
         'statusCode': 200,
-        'body': json.dumps(objects)
+        'body': objects_keys
     }
