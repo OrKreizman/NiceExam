82701261d8aae875dcd48e8c99f6bff24869f002
Or Kreizman <Or.kreizman@mail.huji.ac.il>
2023-12-16 20:19:39 +0200

    Dealing with invalid input and adding documentation

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,74 @@
 import json
-import boto3
+import boto3, botocore
 from datetime import datetime
 
 
-def lambda_handler(event, context):
-    bucket_name = event['bucket name'] if 'bucket name' in event else 'niceexambucket'
+def check_bucket_exists_and_accessible(bucket_name):
+    """
+    Checks the existence and accessibility of an AWS S3 bucket.
+    :param name of the bucket to be checked
+    :return: True if the bucket exists and is accessible, False otherwise
+    """
+    s3 = boto3.resource('s3')
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+        return True
+    except (botocore.exceptions.ClientError, botocore.exceptions.ParamValidationError) as e:
+        # 403 for private 404 does not exist
+        return False
+
+
+def list_bucket_objects(bucket_name):
+    """
+    List all objects in a given S3 bucket.
+    :param bucket_name: The name of the s3 bucket
+    :return:all objects in a given S3 bucket
+    """
     s3_resource = boto3.resource('s3')
-    bucket = s3_resource.Bucket(bucket_name)
-    objects_keys = [obj.key for obj in bucket.objects.all()]
+    given_bucket = s3_resource.Bucket(bucket_name)
+    if not check_bucket_exists_and_accessible(bucket_name): return {'statusCode': 400, 'body': json.dumps(
+        'The given bucket does not exist')}
+    objects_keys = [obj.key for obj in given_bucket.objects.all()]
     if len(objects_keys) == 0:  # bucket is empty
-        objects_keys = [f'No objects found in bucket{bucket_name}']
-    objects_keys = json.dumps(objects_keys)
-
-    # give each object name that includes his creation time
-    cur_time = datetime.now().strftime("%b %d, %Y %H:%M:%S")
-    new_object_name = f'bucket_objects_at:{cur_time}.json'
-
-    bucket.put_object(Key=new_object_name, Body=objects_keys)
-
+        objects_keys = [f'No objects found in bucket: {bucket_name}']
     return {
         'statusCode': 200,
-        'body': objects_keys
+        'body': json.dumps(objects_keys)
     }
+
+
+def store_input_output(bucket_name, storing_bucket_name, response):
+    """
+    Storing input/output data into the bucket <storing_bucket_name>
+    :param bucket_name: The given bucket (input)
+    :param storing_bucket_name: Bucket to store the data in
+    :param objects_keys: The given bucket objects (output)
+    :return:
+    """
+    objects_keys = response['body']
+    s3_resource = boto3.resource('s3')
+    storing_bucket = s3_resource.Bucket(storing_bucket_name)
+    if not check_bucket_exists_and_accessible(storing_bucket_name): return {'statusCode': 400, 'body': json.dumps(
+        'The storing bucket does not exist')}
+    cur_time = datetime.now().strftime("%b %d, %Y %H:%M:%S")  # give each object name that includes his creation time
+    new_object_name = f'list objects from {bucket_name}, at:{cur_time}.json'
+    storing_bucket.put_object(Key=new_object_name, Body=objects_keys)
+    return response
+
+
+def lambda_handler(event, context):
+    """
+    Lambda function to list objects from an S3 bucket and store them
+    :param event: Dictionary containing information about the event that triggered the function.
+                   Needs to contain 'bucket_name' key.
+                   Optional: 'storing_bucket_name' key for the storing bucket name.
+    :param context: AWS Lambda Context object.
+    :return: Dictionary containing the HTTP status code and the list of object keys.
+    """
+
+    bucket_name = event.get('bucket_name', '')
+    storing_bucket_name = event.get('storing_bucket_name', 'storing.lambda.input.output')
+    response = list_bucket_objects(bucket_name)
+    if not response['statusCode'] == 200: return response
+    response = store_input_output(bucket_name, storing_bucket_name, response)
+    return response
--- tests.py
+++ tests.py
@@ -6,31 +6,61 @@ from lambda_function import lambda_handler
 
 
 class TestLambdaHandler(unittest.TestCase):
+    """
+    Class for testing the function lambda_handler
+    """
 
     @mock_s3
     def setUp(self):
-        self.bucket_name = 'bucket'
+        self.given_bucket_name = 'my.tests.bucket'
+        self.storing_bucket_name = 'storing.lambda.input.output'
         self.s3 = boto3.resource('s3')
 
     @mock_s3
+    def test_invalid_buckets_names(self):
+        """
+        Test for invalid AWS S3 bucket names.
+        :return:
+        """
+        invalid_bucket_name_event = {'bucket_name': ''}
+        invalid_storing_bucket_name_event = {'bucket_name': self.given_bucket_name, 'storing_bucket_name': ''}
+        self.s3.create_bucket(Bucket=self.given_bucket_name)
+        self.assertEqual(400, lambda_handler(invalid_bucket_name_event, None)['statusCode'])
+        self.assertEqual(400, lambda_handler(invalid_storing_bucket_name_event, None)['statusCode'])
+
+    @mock_s3
     def test_lambda_with_empty_bucket(self):
-        self.s3.create_bucket(Bucket=self.bucket_name)
-        event = {'bucket name': self.bucket_name}
+        """
+        Test the response of the lambda function for empty bucket (0 objects).
+        :return:
+        """
+        # creating buckets for test
+        self.s3.create_bucket(Bucket=self.given_bucket_name)
+        self.s3.create_bucket(Bucket=self.storing_bucket_name)
+
+        event = {'bucket_name': self.given_bucket_name}
         response = lambda_handler(event, None)
-        # check if statusCode is OK
         self.assertEqual(response['statusCode'], 200)
         response_body = json.loads(response['body'])
         # check if response body is as expected (a list with the message about ampty bucket)
-        expected_response = [f'No objects found in bucket{self.bucket_name}']
+        expected_response = [f'No objects found in bucket: {self.given_bucket_name}']
         self.assertEqual(response_body, expected_response)
 
     @mock_s3
     def test_lambda_response(self):
-        bucket = self.s3.create_bucket(Bucket=self.bucket_name)
+        """
+        Test Lambda function response for a specified S3 bucket.
+        :return:
+        """
+        # creating buckets for test
+        self.s3.create_bucket(Bucket=self.storing_bucket_name)
+        bucket = self.s3.create_bucket(Bucket=self.given_bucket_name)
+        # adding the given bucket some objects for the test
         objects_names = [f'example_object_{i}.txt' for i in range(3)]
         for obj in objects_names:
             bucket.put_object(Key=obj, Body=b'content')
-        event = {'bucket name': self.bucket_name}
+
+        event = {'bucket_name': self.given_bucket_name}
         response = lambda_handler(event, None)
         objects_keys = json.loads(response['body'])
         self.assertEqual(response['statusCode'], 200)
@@ -38,7 +68,16 @@ class TestLambdaHandler(unittest.TestCase):
 
 
 class TestIntegration(unittest.TestCase):
+    """
+    Class for testing the lambda integration.
+    Contacting with the aws lambda with the name <self.lambda_name>
+    """
+
     def setUp(self):
+        """
+        Initial necessaries variables for testing
+        :return: None
+        """
         self.lambda_name = 'listS3BucketObjects'
         self.lambda_client = boto3.client('lambda', 'eu-west-1')
         self.bucket_name = 'my.tests.bucket'
@@ -46,7 +85,13 @@ class TestIntegration(unittest.TestCase):
         self.bucket = s3_resource.Bucket(self.bucket_name)
 
     def invoke_lambda(self):
-        event = {'bucket name': self.bucket_name}
+        """
+        Invoke an AWS Lambda function and retrieve the JSON response.
+        :return:
+        """
+        event = {'bucket_name': self.bucket_name,
+                 'storing_bucket_name': self.bucket_name
+                 }
         response = self.lambda_client.invoke(
             FunctionName=self.lambda_name,
             InvocationType='RequestResponse',  # Can be 'Event' for asynchronous invocation
@@ -55,28 +100,50 @@ class TestIntegration(unittest.TestCase):
         return json.loads(response['Payload'].read())
 
     def get_objects_keys(self):
+        """
+        Get keys of all objects within the given S3 bucket (self.bucket).
+        :return:
+        """
         return [obj.key for obj in self.bucket.objects.all()]
 
     def test_empty_bucket(self):
-        # delete all objects from testing bucket
+        """
+        Test the response of the lambda for empty bucket (0 objects).
+        :return:
+        """
+        # delete all objects from the testing bucket
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(self.bucket_name)
         bucket.objects.all().delete()
         expected_response = {
             'statusCode': 200,
-            'body': json.dumps([f'No objects found in bucket{self.bucket_name}'])
+            'body': json.dumps([f'No objects found in bucket: {self.bucket_name}'])
         }
         response = self.invoke_lambda()
         self.assertDictEqual(expected_response, response)
 
     def test_lambda_response(self):
+        """
+        Test that the lambda returns the expected response for s3 bucket with objects.
+        the expected response is list with all objects keys in the given S3 bucket
+        :return:
+        """
+        # create an object to make sure bucket isn't empty
+        obj_key = 'testing object_object.txt'
+        self.bucket.put_object(Key=obj_key, Body=b'content')
         objects = self.get_objects_keys()
         response_data = self.invoke_lambda()
         expected_returned_data = json.dumps(objects)
         self.assertEqual(200, response_data['statusCode'])
         self.assertEqual(response_data['body'], expected_returned_data)
+        # delete added object
+        self.bucket.Object(obj_key).delete()
 
     def test_s3_saved_data(self):
+        """
+        Test the data saved in an S3 bucket matches the expected data after invoking a Lambda function.
+        :return:
+        """
         expected_objects = self.invoke_lambda()['body']
         added_object_key = max(self.get_objects_keys())
         added_object = self.bucket.Object(added_object_key).get()
